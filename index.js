f31943935c9e6f06bb60378b31b670ca53c46f68
Vivek Singh <petmongrels@gmail.com>
2021-10-28 13:35:08 +0530

    export login service

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import UserService from "./access/service/UserService";
+import LoginService from "./access/service/LoginService";
 
 export {
-    UserService
+    UserService,
+    LoginService
 };

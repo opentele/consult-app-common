c7944139f0fb02d3ac76ff398320aa4f8700f592
Vivek Singh <petmongrels@gmail.com>
2021-10-29 12:26:19 +0530

    FieldValidator

---
+++ framework/validator/FieldValidator.js
@@ -0,0 +1,8 @@
+export default class DataElementValidator {
+    static emailValidator(email) {
+        const re = /\S+@\S+\.\S+/
+        if (!email) return "Email can't be empty."
+        if (!re.test(email)) return 'Ooops! We need a valid email address.'
+        return ''
+    }
+}
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import UserService from "./access/service/UserService";
 import LoginService from "./access/service/LoginService";
+import FieldValidator from "./framework/validator/FieldValidator";
 
 export {
     UserService,
-    LoginService
+    LoginService,
+    FieldValidator
 };

c42a164b77aa6248f37079f03de44bded996c618
Vivek Singh <petmongrels@gmail.com>
2022-01-13 13:58:40 +0530

    login state to handle password or otp entry

--- access/domain/LoginState.js
+++ access/domain/LoginState.js
@@ -6,6 +6,7 @@ class LoginState {
         this.otpMode = false;
         this.otpTries = 0;
         this.loginFailed = false;
+        this.passwordOrOTP = "";
     }
 
     otpRequested() {
@@ -20,12 +21,37 @@ class LoginState {
         this.passwordMode = true;
     }
 
+    passwordOrOTPChanged(passwordOrOtp) {
+        this.passwordOrOTP = passwordOrOtp;
+    }
+
+    canShowOtpSentMessage() {
+        return this.otpTries === 0 && this.otpMode;
+    }
+
+    canRequestOtpOrSwitchPassword() {
+        return this.canRequestOtp() || this.canSwitchToPassword();
+    }
+
+    canRequestOtp() {
+        return this.passwordMode;
+    }
+
+    canSwitchToPassword() {
+        return this.otpMode;
+    }
+
+    passwordOrOTPProvided() {
+        return !_.isEmpty(this.passwordOrOTP);
+    }
+
     static clone(other) {
         let loginState = new LoginState();
         loginState.passwordMode = other.passwordMode;
         loginState.otpMode = other.otpMode;
         loginState.otpTries = other.otpTries;
         loginState.loginFailed = other.loginFailed;
+        loginState.passwordOrOTP = other.passwordOrOTP;
         return loginState;
     }
 }

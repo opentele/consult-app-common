98bdda3283470cc576dd06024711a68b97bde99f
Vivek Singh <petmongrels@gmail.com>
2022-01-25 21:22:03 +0530

    removed login state. added login method

--- access/service/UserService.js
+++ access/service/UserService.js
@@ -17,13 +17,22 @@ export default class UserService {
         localStorage.removeItem('user');
     }
 
-    static registerOrg(name, orgName, email, mobile, password, authMode, cb, errorHandler) {
-        let org = {name: name, organisationName: orgName, email: email, password: password, mobile: mobile, authMode: authMode};
-        return RC.put(`api/app/organisation`, org, cb, errorHandler);
+    static registerOrg(name, orgName, userId, userIdType, password, cb, errorHandler) {
+        let requestBody = {name: name, organisationName: orgName, password: password};
+        if (userIdType === "email") requestBody.email = userId
+        else requestBody.mobile = userId;
+        return RC.put(`api/app/organisation`, requestBody, cb, errorHandler);
     }
 
     static registerUser(name, email, mobile, authMode, organisationId, cb, errorHandler) {
         let user = {name: name, organisationId: organisationId, email: email, mobile: mobile, authMode: authMode};
         return RC.put(`api/app/user`, user, cb, errorHandler);
+    }
+
+    static login(userId, password, cb, errorHandler) {
+        let postObject = {email: userId, password: password};
+        let encodedObj = _.keys(postObject).map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(postObject[key])}`);
+        let formBody = encodedObj.join("&");
+        return RC.post("api/login", formBody, cb, errorHandler, "xWwwForm");
     }
 }

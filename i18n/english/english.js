735b2aab8e98924b50f144b6eb522a224e588bc3
Vivek Singh <petmongrels@gmail.com>
2022-03-29 18:39:59 +0530

    .

--- i18n/english/english.js
+++ i18n/english/english.js
@@ -4,5 +4,6 @@ import consultationRoom from './consultatonRoom.json';
 import consultation from './consultation.json';
 import system from './system.json';
 import client from './client.json';
+import consultationRoomSession from './consultationRoomSession.json';
 
-export default {...access, ...common, ...consultationRoom, ...consultation, ...system, ...client};
+export default {...access, ...common, ...consultationRoom, ...consultation, ...system, ...client, ...consultationRoomSession};

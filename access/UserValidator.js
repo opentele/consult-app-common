92cb8270e3e6c3ec8276f010d09fe419fdbc4283
Vivek Singh <petmongrels@gmail.com>
2022-08-17 16:02:20 +0530

    .

--- access/UserValidator.js
+++ access/UserValidator.js
@@ -5,7 +5,7 @@ class UserValidator {
     static validate(user, confirmPassword, validatePasswords) {
         const [validUserName, userNameType] = DataElementValidator.validateEmailOrMobileWithCountryCode(user.userName);
         const passwordsValid = validatePasswords ? DataElementValidator.validatePasswords(user.password, confirmPassword) : true;
-        const nameValid = !_.isEmpty(user.name);
+        const nameValid = !_.isEmpty(user.name) && user.name.length <= 100;
         const providerTypeValid = !_.isNil(user.providerType);
 
         if (passwordsValid && validUserName && nameValid && providerTypeValid) return [true, userNameType, {}];
@@ -13,7 +13,7 @@ class UserValidator {
         const errors = {};
         if (!passwordsValid) errors["passwords"] = "password-not-matching";
         if (!validUserName) errors["userName"] = "invalid-user-name";
-        if (!nameValid) errors["name"] = "name-empty";
+        if (!nameValid) errors["name"] = "name-size-invalid";
         if (!providerTypeValid) errors["providerType"] = "provider-type-empty";
         return [false, userNameType, errors];
     }

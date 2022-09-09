c6867b57847d6c70192292e3f5f3ceb04bca38d6
Vivek Singh <petmongrels@gmail.com>
2022-09-09 12:57:24 +0530

    .

--- i18n/hindi/english.test.js
+++
@@ -1,6 +0,0 @@
-import english from './hindi';
-
-test('should translate', () => {
-    expect(english['full-name']).toBe("Full name");
-    expect(english['conference-client-next']).not.toBeNull();
-});

faec144834b0ebe94d52588335112743f1006e61
Vivek Singh <petmongrels@gmail.com>
2023-02-10 15:42:55 +0530

    .

--- access/domain/ProviderType.js
+++ access/domain/ProviderType.js
@@ -1,8 +1,12 @@
-import _ from "lodash";
-
-export default {
+const ProviderType = {
     Moderator: 'Moderator',
     Consultant: 'Consultant',
     None: 'None',
     Both: 'Both'
 };
+
+export default ProviderType;
+
+export function canAccessClientRecords(providerType) {
+    return providerType !== ProviderType.None;
+}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import UserService from "./access/service/UserService.js";
 import i18n, {i18nPromise} from './i18n/loc.js';
-import ProviderType from './access/domain/ProviderType.js';
+import ProviderType, {canAccessClientRecords} from './access/domain/ProviderType.js';
 import UserValidator from './access/UserValidator.js';
 import User from './access/domain/User.js';
 import AbstractEntity from './access/domain/AbstractEntity.js';
@@ -25,5 +25,6 @@ export {
     User,
     AbstractEntity,
     RRuleTranslations,
-    RRuleStrTranslations
+    RRuleStrTranslations,
+    canAccessClientRecords
 };
